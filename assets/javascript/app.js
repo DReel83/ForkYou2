fc1ddacce61e918a1a69b88c34dbe87b926d3364
David Reel <davidpreel@gmail.com>
2016-08-11 21:27:29 -0400

    message

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+function displayRecipe()
+ 	{
+
+ 		$('#recipeAppearHere').empty();
+        var recipe = $(this).data('name');
+        console.log(recipe);
+        var queryURL = "http://food2fork.com/api/get?key=c3efaeda1af2d39309095a618614e527&q=";
+        console.log(queryURL);
+        $.ajax(
+        {
+                url: queryURL,
+                method: 'GET'
+        })
+            .done(function(response) 
+            {
+
+                console.log(response)
+            })
+    };
---
+++ assets/javascript/yelpapi.js
@@ -0,0 +1,5 @@
+$('#submit').on('click', function() {
+    console.log('clicked');
+
+return false
+});

20f5835d0802affe6e7d9c464baa2b20675e5a4a
Melissa Holsapple <melissaholsapple@Roadhouse.local>
2016-08-11 20:55:26 -0400

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  path = require('path');
 
 var port = process.env.PORT || 8080;
 
@@ -8,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function(req, res) {
    // ejs render automatically looks in the views folder
-   res.sendFile(path.join(__dirname+'public/index.html'));
+   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(port, function() {

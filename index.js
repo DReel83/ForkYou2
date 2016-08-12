6fc4d417720348f7aecb863523ba885e68820913
Melissa Holsapple <melissaholsapple@Roadhouse.local>
2016-08-11 21:01:03 -0400

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express(),
+path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

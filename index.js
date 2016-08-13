921c8c427dc7efe3f9dd25bc561aba7aae07b32e
Melissa Holsapple <melissaholsapple@Roadhouse.local>
2016-08-13 10:07:16 -0400

    stable

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express(),
 path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
 
 
-app.all('/*', function(req, res, next) {
+app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
